7e5ce5e9e506c10a18a75270f103452018eb5137
wetzel7 <loganpsavage15@gmail.com>
2026-01-27 21:36:11 -0500

    week2

---
+++ Week2/PMpart4.ps1
@@ -0,0 +1,17 @@
+﻿# PowerShell Script to start an instance of Google Chrome and directs to Champlain.edu
+# If chrome is already running, it kills it
+
+# getting process
+$processName = "chrome"
+$Process = Get-Process -Name $ProcessName -ErrorAction SilentlyContinue
+
+# Killing chrome if running
+if ($process){
+    Write-Host "$ProcessName is closing"
+       get-process -Name "chrome" | stop-process
+} 
+# Starting chrome if not running
+else {
+    Write-Host "Starting $processName"
+        Start-Process "chrome.exe" "https://champlain.edu"
+    }
